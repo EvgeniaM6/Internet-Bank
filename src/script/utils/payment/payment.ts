f0cfaad8fa783cf6620d3a62b75600f53d82329a
EvgeniaM6 <evgminukova6@gmail.com>
2023-02-01 00:19:21 +0200

    feat: create get token method and take commission for anonim

--- src/script/utils/payment/payment.ts
+++ src/script/utils/payment/payment.ts
@@ -1,18 +1,32 @@
+import { COMMISSION_AMOUNT } from '../../data/constants';
 import { EOperation } from '../../data/types';
 import { moneyFetch } from '../../fetch/moneyFetch';
+import { userFetch } from '../../fetch/userFetch';
 
 class Payment {
   constructor() {
     console.log('Payment');
+    sessionStorage.setItem(
+      'token',
+      'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZDdmNWE4NTMzMzJlYTI3YmZhMzQ4MyIsImlhdCI6MTY3NTA5Nzc0NH0.3WDMi60yspkcXcKABMM-waYSwzqhl-SEq1FZsE4chRI'
+    );
   }
 
   makePayment(sum: number, operationID: number): void {
     const token = this.getCurrentToken();
-    moneyFetch.changeMainMoney(sum, EOperation.REMOVE, token, operationID);
+    if (token) {
+      moneyFetch.changeMainMoney(sum, EOperation.REMOVE, token, operationID);
+      userFetch.isOurUser('evg').then((resp) => {
+        console.log('resp evg =', resp);
+      });
+    } else {
+      const commissSum = COMMISSION_AMOUNT;
+      moneyFetch.commission(commissSum, operationID);
+    }
   }
 
   getCurrentToken(): string {
-    return '';
+    return sessionStorage.getItem('token') || '';
   }
 }
 
