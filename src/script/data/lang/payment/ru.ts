f414383fb38218c0edafca9c80970b0ab18d24df
EvgeniaM6 <evgminukova6@gmail.com>
2023-02-05 01:06:05 +0200

    feat: add message for confirmed payment

--- src/script/data/lang/payment/ru.ts
+++ src/script/data/lang/payment/ru.ts
@@ -1,5 +1,14 @@
+import { COMMISSION_AMOUNT } from '../../constants';
+
 export default {
   'card__category-text': 'Категория:',
   filter__title: 'Выберите категорию:',
   card__btn: 'оплатить',
+  'operation__category-text': 'Категория:',
+  'form-paym__btn': 'оплатить со своего счета',
+  'form-paym__btn-card': 'оплатить картой',
+  operation__commission: `Комиссия за данную операцию составляет ${COMMISSION_AMOUNT} дол.США`,
+  paidByCardMessage: 'Ваш платеж подтвержден',
+  errorPayByCardMessage: 'Упс! Что-то пошло не так',
+  back__text: 'Вернуться ко всем сервисам',
 };
