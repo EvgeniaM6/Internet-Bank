177abdd79d20eca6b782306d24ed4c49b5ef0207
EvgeniaM6 <evgminukova6@gmail.com>
2023-02-04 13:33:41 +0200

    feat: run rendering services payment page

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
-import { buildAuth } from './script/utils/auth/buildAuth';
-import { createAuth } from './script/utils/auth/createAuth';
-import { buildMain } from './script/utils/main/buildMain';
+// import { buildAuth } from './script/utils/auth/buildAuth';
+// import { createAuth } from './script/utils/auth/createAuth';
+// import { buildMain } from './script/utils/main/buildMain';
+import { renderPayment } from './script/utils/payment/renderPayment';
 import './styles/style.scss';
 
-buildAuth.main();
-createAuth.login();
+// buildAuth.main();
+// createAuth.login();
 //createAuth.registration();
 //createAuth.reset();
 //createAuth.verify();
@@ -15,3 +16,4 @@ createAuth.login();
 /*window.onbeforeunload = () => {
   sessionStorage.removeItem('token');
 };*/
+renderPayment.renderPaymentsPage();
