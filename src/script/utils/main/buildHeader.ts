453ecc1e30c92947da00edb2112430138cb27f0a
Yahor Litavar <sergioegor21@gmail.com>
2023-02-13 14:08:07 +0200

    feat: auth lang and global refactoring

--- src/script/utils/main/buildHeader.ts
+++ src/script/utils/main/buildHeader.ts
@@ -5,6 +5,7 @@ class BuildHeader {
   anonimHeader() {
     const header = document.querySelector('header');
     const main = document.querySelector('.main');
+    const isEnglish = config.lang === 'en';
     if (!header || !(main instanceof HTMLElement)) return;
 
     //if (document.documentElement.clientWidth > 767) main.style.marginLeft = '230px';
@@ -13,24 +14,20 @@ class BuildHeader {
     header.innerHTML = `<div class="header__up">
         <h1 class="header__logo">RS Bank</h1>
     </div>
-    <button class="header__burger">${config.lang === 'en' ? 'Menu' : 'Меню'}</button>
+    <button class="header__burger">${isEnglish ? 'Menu' : 'Меню'}</button>
     <nav class="header__nav">
         <ul class="header__ul">
-            <li class="header__nav-item header__nav-about" id="about">${
-              config.lang === 'en' ? 'About' : 'О Проекте'
-            }</li>
-            <li class="header__nav-item header__nav-services" id="services">${
-              config.lang === 'en' ? 'Services' : 'Услуги'
-            }</li>
-            <li class="header__nav-item header__nav-quiz" id="quiz">${config.lang === 'en' ? 'Quiz' : 'Квиз'}</li>
+            <li class="header__nav-item header__nav-about" id="about">${isEnglish ? 'About' : 'О Проекте'}</li>
+            <li class="header__nav-item header__nav-services" id="services">${isEnglish ? 'Services' : 'Услуги'}</li>
+            <li class="header__nav-item header__nav-quiz" id="quiz">${isEnglish ? 'Quiz' : 'Квиз'}</li>
             <li class="header__nav-item header__nav-stat" id="statistics">${
-              config.lang === 'en' ? 'Statistics' : 'Статистика'
+              isEnglish ? 'Statistics' : 'Статистика'
             }</li>
         </ul>
         <button class="header__burger-close">&#10006;</button>
     </nav>
     <div class="header__down">
-        <div class="header__login">${config.lang === 'en' ? 'Log In' : 'Войти'}</div>
+        <div class="header__login">${isEnglish ? 'Log In' : 'Войти'}</div>
         <div class="header__switch_theme">
           <img src="${moon}" alt="moon" class="header__theme">
         </div>
@@ -42,27 +39,28 @@ class BuildHeader {
     const list = document.querySelector('.header__ul');
     const logout = document.querySelector('.header__login');
     const headerUp = document.querySelector('.header__up');
+    const isEnglish = config.lang === 'en';
     if (!list || !logout || !headerUp) return;
 
-    logout.textContent = config.lang === 'en' ? 'Log Out' : 'Выйти';
+    logout.textContent = isEnglish ? 'Log Out' : 'Выйти';
     logout.classList.add('header__logout');
 
     const account = document.createElement('li');
     account.classList.add('header__nav-item');
     account.classList.add('header__nav-account');
-    account.textContent = config.lang === 'en' ? 'Account' : 'Аккаунт';
+    account.textContent = isEnglish ? 'Account' : 'Аккаунт';
     account.id = 'account';
 
     const cardCreator = document.createElement('li');
     cardCreator.classList.add('header__nav-item');
     cardCreator.classList.add('header__nav-card');
-    cardCreator.textContent = config.lang === 'en' ? 'Card Creator' : 'Создать карточку';
+    cardCreator.textContent = isEnglish ? 'Card Creator' : 'Создать карточку';
     cardCreator.id = 'card';
 
     const stocks = document.createElement('li');
     stocks.classList.add('header__nav-item');
     stocks.classList.add('header__nav-stocks');
-    stocks.textContent = config.lang === 'en' ? 'Stocks' : 'Акции';
+    stocks.textContent = isEnglish ? 'Stocks' : 'Акции';
     stocks.id = 'stocks';
 
     const money = document.createElement('p');
@@ -81,12 +79,13 @@ class BuildHeader {
   adminHeader() {
     this.logHeader();
     const list = document.querySelector('.header__ul');
+    const isEnglish = config.lang === 'en';
     if (!list) return;
 
     const admin = document.createElement('li');
     admin.classList.add('header__nav-item');
     admin.classList.add('header__nav-admin');
-    admin.textContent = config.lang === 'en' ? 'Administration' : 'Админка';
+    admin.textContent = isEnglish ? 'Administration' : 'Админка';
     admin.id = 'admin';
 
     list.appendChild(admin);
--- src/script/utils/stocks/buildStocks.ts
+++ src/script/utils/stocks/buildStocks.ts
@@ -14,23 +14,26 @@ class BuildStock {
 
   buildUserList(userStocks: IUserStocks[], marketStocks: IMarketStocks[]) {
     const userList = document.querySelector('.stocks__user');
+    const isEnglish = config.lang === 'en';
+
     if (!userList) return;
 
     if (!userStocks.length) {
-      userList.innerHTML = `<h3 class="stocks__user-h">${config.lang === 'en' ? 'User Stocks' : 'Ваши Акции'}</h3>
-      <p class="stocks__user-empty">${config.lang === 'en' ? "You haven't any stocks" : 'У вас нет акций'}</p>`;
+      userList.innerHTML = `<h3 class="stocks__user-h">${isEnglish ? 'User Stocks' : 'Ваши Акции'}</h3>
+      <p class="stocks__user-empty">${isEnglish ? "You haven't any stocks" : 'У вас нет акций'}</p>`;
       return;
     }
 
-    userList.innerHTML = `<h3 class="stocks__user-h">${config.lang === 'en' ? 'User Stocks' : 'Ваши Акции'}</h3>
+    userList.innerHTML = `<h3 class="stocks__user-h">${isEnglish ? 'User Stocks' : 'Ваши Акции'}</h3>
     <p class="stocks__user-header">
-        <span class="stocks__user-header-name">${config.lang === 'en' ? 'Name' : 'Название'}</span>
-        <span class="stocks__user-header-count">${config.lang === 'en' ? 'Count' : 'Кол-во'}</span>
-        <span class="stocks__user-header-profit">${config.lang === 'en' ? 'Profit' : 'Прибыль'}</span>
+        <span class="stocks__user-header-name">${isEnglish ? 'Name' : 'Название'}</span>
+        <span class="stocks__user-header-count">${isEnglish ? 'Count' : 'Кол-во'}</span>
+        <span class="stocks__user-header-profit">${isEnglish ? 'Profit' : 'Прибыль'}</span>
     </p>`;
 
     userStocks.forEach((el) => {
       const stock = marketStocks.find((stock) => el.name === stock.name);
+      const isEnglish = config.lang === 'en';
       if (!stock) return;
 
       const profit = stock.money - el.price;
@@ -48,8 +51,8 @@ class BuildStock {
           <button class="item__plus">+</button>
       </div>
       <div class="stocks__user-payment">
-          <button class="stocks__user-button stocks__button">${config.lang === 'en' ? 'Sell' : 'Продать'}</button>
-          <p class="stocks__user-status">${config.lang === 'en' ? 'Ready to deal' : 'Открыто'}</p>
+          <button class="stocks__user-button stocks__button">${isEnglish ? 'Sell' : 'Продать'}</button>
+          <p class="stocks__user-status">${isEnglish ? 'Ready to deal' : 'Открыто'}</p>
       </div>`;
 
       userList.appendChild(div);
@@ -58,13 +61,14 @@ class BuildStock {
 
   buildMarketList(marketStocks: IMarketStocks[]) {
     const marketList = document.querySelector('.stocks__market');
+    const isEnglish = config.lang === 'en';
     if (!marketList) return;
 
-    marketList.innerHTML = `<h3 class="stocks__market-h">${config.lang === 'en' ? 'Stocks Market' : 'Биржа'}</h3>
+    marketList.innerHTML = `<h3 class="stocks__market-h">${isEnglish ? 'Stocks Market' : 'Биржа'}</h3>
     <p class="stocks__market-header">
-        <span class="stocks__market-header-name">${config.lang === 'en' ? 'Name' : 'Название'}</span>
-        <span class="stocks__market-header-count">${config.lang === 'en' ? 'Count' : 'Кол-во'}</span>
-        <span class="stocks__market-header-price">${config.lang === 'en' ? 'Price' : 'Цена'}</span>
+        <span class="stocks__market-header-name">${isEnglish ? 'Name' : 'Название'}</span>
+        <span class="stocks__market-header-count">${isEnglish ? 'Count' : 'Кол-во'}</span>
+        <span class="stocks__market-header-price">${isEnglish ? 'Price' : 'Цена'}</span>
     </p>`;
 
     marketStocks.forEach((el) => {
@@ -80,8 +84,8 @@ class BuildStock {
           <button class="item__plus">+</button>
       </div>
       <div class="stocks__market-payment">
-        <button class="stocks__market-button stocks__button">${config.lang === 'en' ? 'Buy' : 'Купить'}</button>
-        <p class="stocks__market-status">${config.lang === 'en' ? 'Ready to deal' : 'Открыто'}<p>
+        <button class="stocks__market-button stocks__button">${isEnglish ? 'Buy' : 'Купить'}</button>
+        <p class="stocks__market-status">${isEnglish ? 'Ready to deal' : 'Открыто'}<p>
       </div>`;
 
       marketList.appendChild(div);
