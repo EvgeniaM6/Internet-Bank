392915a1d8f6a2816e60ec6bc9ae79c649c645ee
Yahor Litavar <sergioegor21@gmail.com>
2023-02-24 14:05:10 +0200

    add favicon

--- webpack.config.js
+++ webpack.config.js
@@ -43,11 +43,11 @@ module.exports = {
     }),
     new CleanWebpackPlugin(),
     new EslintPlugin({extensions: 'ts'}),
-    /* new CopyPlugin({
+    /*new CopyPlugin({
       patterns: [
         { from: 'src/assets', to: 'assets'},
       ],
-    }), */
+    }),*/
     new NetlifyPlugin({redirects: [
         {
           from: "/*",
